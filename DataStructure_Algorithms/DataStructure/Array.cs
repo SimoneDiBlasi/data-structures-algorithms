d8de8eb9af0963bfc9392fdc4fab08702715c8d5
SimoneDiBlasi <simo_dibla@hotmail.it>
2024-06-10 09:59:16 +0200

    init

---
+++ DataStructure_Algorithms/DataStructure/Array.cs
@@ -0,0 +1,27 @@
+﻿namespace DataStructure
+{
+    public class ArrayClass(int[] array)
+    {
+        public int[] Array { get; set; } = array;
+
+        public int Find(int index) // O(1)
+        {
+            return Array[index];
+        }
+
+        public void Print() // O(n)
+        {
+            Console.WriteLine(Array);
+        }
+
+        public int Max() // O(n)
+        {
+            return Array.Max();
+        }
+
+        public int[] Reverse() // O(n)
+        {
+            return Array.Reverse().ToArray();
+        }
+    }
+}
