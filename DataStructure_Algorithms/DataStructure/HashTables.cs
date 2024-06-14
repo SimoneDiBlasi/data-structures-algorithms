1ef98e865a8fa27e49e993bfbb76b38dafcd750f
SimoneDiBlasi <s.diblasi@agricolus.com>
2024-06-14 16:50:17 +0200

    queue, dictionary

---
+++ DataStructure_Algorithms/DataStructure/HashTables.cs
@@ -0,0 +1,23 @@
+﻿namespace DataStructure
+{
+    public class HashTableClass(Dictionary<int, string> dictionary)
+    {
+        public Dictionary<int, string> HashTable { get; set; } = dictionary;
+
+        public void Add(int key, string item)
+        {
+            HashTable.Add(key, item); //O(1)
+        }
+
+        public void Delete(int key)
+        {
+            HashTable.Remove(key); //O(1)
+        }
+
+
+        public void Search(int key)
+        {
+            HashTable.GetValueOrDefault(key, null); //O(1)
+        }
+    }
+}
---
+++ DataStructure_Algorithms/DataStructure/Stack.cs
@@ -0,0 +1,24 @@
+﻿namespace DataStructure
+{
+    public class StackClass(Stack<int> stack)
+    {
+        // Stack(LIFO) -> Solitamanete ultizzati per algoritmi di backtracking -> Ti permette di tornare indietro tramire 
+        public Stack<int> Stack { get; set; } = stack;
+
+        public void Add(int item)
+        {
+            Stack.Push(item); //O(1)
+        }
+
+        public void Delete()
+        {
+            Stack.Pop();  //O(1)
+        }
+
+
+        public void Peek()
+        {
+            Stack.Peek(); //O(1)
+        }
+    }
+}
