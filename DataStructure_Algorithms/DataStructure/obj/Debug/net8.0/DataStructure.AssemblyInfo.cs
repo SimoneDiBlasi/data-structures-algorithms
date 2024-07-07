eb7d346e29f13c62dc3038a62891556dc0a49a50
SimoneDiBlasi <s.diblasi@agricolus.com>
2024-07-07 15:36:19 +0200

    Trees

--- DataStructure_Algorithms/DataStructure/obj/Debug/net8.0/DataStructure.AssemblyInfo.cs
+++ DataStructure_Algorithms/DataStructure/obj/Debug/net8.0/DataStructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataStructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ef98e865a8fa27e49e993bfbb76b38dafcd750f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataStructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataStructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
